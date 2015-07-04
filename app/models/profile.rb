d7e799b53893602a2f10b35e368e3f5e8429c1c0
aura <aura@esplice.com>
2015-07-04 13:23:03 +1000

    added business profiels

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,5 @@ class Profile < ActiveRecord::Base
   belongs_to :user
 
   mount_uploader :profile_pic, ProfilePicUploader
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
-class User < ActiveRecord::Base
-	has_one :profile
-  rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+class User < ActiveRecord::Base   
+has_one :profile   
+has_many :business_users
+has_many :business_profiles, through: :business_users   
+rolify   
+# Include default devise modules. Others available are:   
+# :confirmable, :lockable,:timeoutable and :omniauthable   
+devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   after_create :assign_resident_role
 
